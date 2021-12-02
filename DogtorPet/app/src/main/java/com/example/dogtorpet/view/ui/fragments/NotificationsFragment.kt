abbbda54f43c90ce6b4e7c96858b5e4ee8f8300c
Joaron4 <82114258+Joaron4@users.noreply.github.com>
2021-12-02 13:31:21 -0500

    material desing

--- DogtorPet/app/src/main/java/com/example/dogtorpet/view/ui/fragments/NotificationsFragment.kt
+++ DogtorPet/app/src/main/java/com/example/dogtorpet/view/ui/fragments/NotificationsFragment.kt
@@ -9,11 +9,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.dogtorpet.databinding.FragmentNotificationsBinding
-import com.example.dogtorpet.viewmodel.NotificationsViewModel
+import com.example.dogtorpet.viewmodel.OrderViewModel
 
 class NotificationsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var orderViewModel: OrderViewModel
     private var _binding: FragmentNotificationsBinding? = null
 
     // This property is only valid between onCreateView and
@@ -25,14 +25,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        orderViewModel =
+            ViewModelProvider(this).get(OrderViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        orderViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
