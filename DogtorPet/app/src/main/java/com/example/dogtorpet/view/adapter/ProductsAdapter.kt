4b77069242603582d664f5b677ab8fb315d80e2e
Joaron4 <82114258+Joaron4@users.noreply.github.com>
2021-12-07 10:21:17 -0500

    conexión firestore

---
+++ DogtorPet/app/src/main/java/com/example/dogtorpet/view/adapter/ProductsAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.dogtorpet.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dogtorpet.R
+import com.example.dogtorpet.model.Products
+import com.example.dogtorpet.view.ui.fragments.HomeFragment
+import com.squareup.picasso.Picasso
+
+
+class ProductsAdapter(val productsListener: ProductsListener) : RecyclerView.Adapter<ProductsAdapter.ViewHolder> () {
+
+    var listProducts = ArrayList<Products>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.products_card,parent,false))
+
+    override fun onBindViewHolder(holder: ProductsAdapter.ViewHolder, position: Int) {
+        val products = listProducts[position]
+
+        holder.tvNameProduct.text = products.nombre
+        holder.tvPriceProduct.text = products.precio.toString()
+        Picasso.get().load(products.url).into(holder.ivItemProduct)
+
+        holder.itemView.setOnClickListener {
+            productsListener.OnProductsClick(products, position)
+        }
+    }
+
+    override fun getItemCount()= listProducts.size
+
+    fun updateData(data: List<Products>) {
+        listProducts.clear()
+        listProducts.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvNameProduct = itemView.findViewById<TextView>(R.id.productName)
+        val tvPriceProduct = itemView.findViewById<TextView>(R.id.productPrice)
+        val ivItemProduct = itemView.findViewById<ImageView>(R.id.productImage)
+    }
+}
--- DogtorPet/app/src/main/java/com/example/dogtorpet/viewmodel/HomeViewModel.kt
+++ DogtorPet/app/src/main/java/com/example/dogtorpet/viewmodel/HomeViewModel.kt
@@ -3,8 +3,38 @@ package com.example.dogtorpet.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.dogtorpet.model.Products
+import com.example.dogtorpet.network.Callback
+import com.example.dogtorpet.network.FirestoreService
+import java.lang.Exception
 
 class HomeViewModel : ViewModel() {
+    val firestoreService = FirestoreService()
+    val listProducts: MutableLiveData<List<Products>> = MutableLiveData()
+    var isLoading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        getProductsFromFirebase()
+    }
+
+    private fun getProductsFromFirebase() {
+        firestoreService.getProducts(object : Callback<List<Products>> {
+
+            override fun onFailed(exception: Exception) {
+                processFinished()
+            }
+
+            override fun onSuccess(result: List<Products>?) {
+                listProducts.postValue(result)
+                processFinished()
+            }
+
+        })
+    }
+
+    fun processFinished() {
+        isLoading.value = true
+    }
 
     private val _text = MutableLiveData<String>().apply {
         value = "Hola,Usuario"
